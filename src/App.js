31b9c2126da42f77fb826142669a5a63f210b7fa
seolleung2 <seolleung22@gmail.com>
2020-11-20 21:01:58 +0900

    #1.3 Added key when i use map function it is important to make element unique

--- src/App.js
+++ src/App.js
@@ -11,26 +11,31 @@ function Food(props) {
 
 const foodIlike = [
   {
+    id: 1,
     name: "Samgyupsal🍖",
     image:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmp-seoul-image-production-s3.mangoplate.com%2Fmango_pick%2Fuker6u9xhkr1m8.jpg&f=1&nofb=1",
   },
   {
+    id: 2,
     name: "Wine🍷",
     image:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ft1.daumcdn.net%2Fthumb%2FR1280x0%2F%3Ffname%3Dhttp%3A%2F%2Ft1.daumcdn.net%2Fbrunch%2Fservice%2Fuser%2Fciv%2Fimage%2FyPsX8Xxl95iuasNOrNt--Gr0aoQ&f=1&nofb=1",
   },
   {
+    id: 3,
     name: "pizza🍕",
     image:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F243BC34358D0CAEB17&f=1&nofb=1",
   },
   {
+    id: 4,
     name: "fried potato🍟",
     image:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdnweb01.wikitree.co.kr%2Fwebdata%2Feditor%2F201604%2F20%2Fimg_20160420093450_49ea2f90.jpg&f=1&nofb=1",
   },
   {
+    id: 5,
     name: "jokbal🍗",
     image:
       "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fmbymadmin.reddot.kr%2Fdata%2Fupload%2F10153%2Fvisual3.jpg&f=1&nofb=1",
@@ -48,7 +53,7 @@ function App() {
       <Food fav="fried potato🍟" />
       <Food fav="jokbal🍗" /> */}
       {foodIlike.map((food) => {
-        return <Food name={food.name} url={food.image} />;
+        return <Food key={food.id} name={food.name} url={food.image} />;
       })}
     </div>
   );
