82538155f8b9622fdc4047639044b2960cb603d6
seolleung2 <seolleung22@gmail.com>
2020-11-21 17:48:03 +0900

    #1.7 Movie app begins

--- src/App.js
+++ src/App.js
@@ -1,48 +1,19 @@
 import React from "react";
-// import PropTypes from "prop-types";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("Constructor Hello");
-  }
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
-  add = () => {
-    // console.log("I Added!");
-    // this.setState({ count: this.state.count + 1 });
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-
-  minus = () => {
-    // console.log("Minus!");
-    // this.setState({ count: this.state.count - 1 });
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
-  // 1. Component 하나가 태어날 때 (Component 호출 👉🏾 render 실행 👉🏾 componentDidMount 실행)
   componentDidMount() {
-    console.log("Component rendered");
-  }
-
-  // 2. Component 가 업데이트 될 때 (상태변화)
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-  // 3. Component 가 죽을 때, 다른 페이지로 넘어가거 나 하는 이유로..
-  componentWillUnmount() {
-    console.log("Goodbye, cruel world");
+    setTimeout(() => {
+      // this.setState((current) => ({ isLoading: false }));
+      this.setState({ isLoading: false });
+    }, 6000);
   }
   render() {
-    console.log("I'm rendering..");
-    return (
-      <div>
-        <h1>Number Counter 👉🏾 {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
-// 매 순간 내가 setState 를 호출할 때마다 react 는 새로운 state 와 함께 render function 을 호출하게 된다! 리렌더 하는거다!
 export default App;
